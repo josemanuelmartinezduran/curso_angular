bfa44c7077054fae13ef84eb4e720cc9e115f40f
Jose M  Martz <zhiker@leah>
2023-09-21 12:59:41 -0600

    Popup

--- src/app/components/dialogs/add-product/add-product.component.ts
+++ src/app/components/dialogs/add-product/add-product.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Producto } from 'src/app/models/producto';
 
 @Component({
@@ -7,15 +7,37 @@ import { Producto } from 'src/app/models/producto';
   templateUrl: './add-product.component.html',
   styleUrls: ['./add-product.component.css'],
 })
-export class AddProductComponent {
+export class AddProductComponent implements OnInit{
   descripcion: String = '';
   colores: String = '';
   clave: String = '';
   precio: number = 0;
+  errorText = "";
 
-  constructor(public dialogRef: MatDialogRef<AddProductComponent>) {}
+  constructor(public dialogRef: MatDialogRef<AddProductComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,) {}
+
+  ngOnInit(): void {
+    const producto:Producto = this.data.producto;
+    this.descripcion = producto.descripcion;
+    this.colores = producto.colores;
+    this.clave = producto.clave;
+    this.precio = producto.precio;
+  }
 
   onAddProduct() {
+    if(this.descripcion == "")
+      this.errorText = "Debe colocar una descripción"
+    if(this.colores == "")
+      this.errorText += "Debe colocar al menos un color"
+    if(this.precio <= 0)
+      this.errorText += "El precio no puede ser menor o igual a cero"
+    if(this.clave == "")
+      this.errorText += "La clave no puede estar vacía"
+    if(this.errorText != ""){
+      alert("Revise el formulario");
+      return;
+    }
     this.dialogRef.close(
       new Producto(
         this.descripcion as string,
--- src/app/components/pages/pipes/pipes.component.ts
+++ src/app/components/pages/pipes/pipes.component.ts
@@ -6,20 +6,23 @@ import { AddProductComponent } from '../../dialogs/add-product/add-product.compo
 @Component({
   selector: 'app-pipes',
   templateUrl: './pipes.component.html',
-  styleUrls: ['./pipes.component.css']
+  styleUrls: ['./pipes.component.css'],
 })
-export class PipesComponent implements OnInit{
-  list:Producto[] = Array();
+export class PipesComponent implements OnInit {
+  list: Producto[] = Array();
 
-  constructor(private dialog:MatDialog){}
+  constructor(private dialog: MatDialog) {}
 
-  addProduct(){
+  addProduct() {
     const dialogRef = this.dialog.open(AddProductComponent);
-    dialogRef.afterClosed().subscribe((value:any)=>{
-      console.log("Dialogo cerrado")
-      const producto:Producto = value as unknown as Producto;
-      this.list.push(producto);
-      console.log(producto);
+    dialogRef.afterClosed().subscribe((value: any) => {
+      if (value != undefined && value != "") {
+        console.log('Dialogo cerrado');
+        console.log(typeof value);
+        const producto: Producto = value as unknown as Producto;
+        this.list.push(producto);
+        console.log();
+      }
     });
   }
 
@@ -27,18 +30,22 @@ export class PipesComponent implements OnInit{
     this.getData();
   }
 
-  getData(){
-    this.list.push(new Producto("Lapiz #2", "LL02", "Verde,Amarillo", 450.00));
-    this.list.push(new Producto("Goma", "GG0302", "Cyan,Magenta", 15.00));
-    this.list.push(new Producto("Zacapuntas", "GG0302", "Verde,Azul", 15.00));
-    this.list.push(new Producto("Boligrafo", "GG0302", "Amarillo,Rojo", 15.00));
+  getData() {
+    this.list.push(new Producto('Lapiz #2', 'LL02', 'Verde,Amarillo', 450.0));
+    this.list.push(new Producto('Goma', 'GG0302', 'Cyan,Magenta', 15.0));
+    this.list.push(new Producto('Zacapuntas', 'GG0302', 'Verde,Azul', 15.0));
+    this.list.push(new Producto('Boligrafo', 'GG0302', 'Amarillo,Rojo', 15.0));
   }
 
-  edit(id:any){
-
+  edit(id: number) {
+    const dialogRef = this.dialog.open(AddProductComponent, {
+      data: {
+        producto: this.list[id]
+      }
+    });
   }
 
-  remove(id:any){
-
+  remove(id: number) {
+    this.list.splice(id, 1);
   }
 }
